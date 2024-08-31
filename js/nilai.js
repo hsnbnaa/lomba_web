899769968d5abcabaf34d86f84c12988b4211a96
Hasan Al Banna <lalbannal13@gmail.com>
2024-08-31 13:25:16 +0700

    s

--- js/nilai.js
+++ js/nilai.js
@@ -6,12 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
   ).then((response) => {
     response.json().then((data) => {
       result = data[0];
-      document.getElementById("ul-1").textContent = result.ulangan_1;
-      document.getElementById("ul-2").textContent = result.ulangan_2;
-      document.getElementById("ul-3").textContent = result.ulangan_3;
-      document.getElementById("ul-4").textContent = result.ulangan_4;
-      document.getElementById("uts").textContent = result.uts;
-      document.getElementById("uas").textContent = result.uas;
+      document.getElementById("ul-1").textContent = result.ulangan_1.toFixed(2);
+      document.getElementById("ul-2").textContent = result.ulangan_2.toFixed(2);
+      document.getElementById("ul-3").textContent = result.ulangan_3.toFixed(2);
+      document.getElementById("ul-4").textContent = result.ulangan_4.toFixed(2);
+      document.getElementById("uts").textContent = result.uts.toFixed(2);
+      document.getElementById("uas").textContent = result.uas.toFixed(2);
     });
   });
 });
